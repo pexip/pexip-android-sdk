dabc1c22664caf605a333d16ec67ce33fdfbe1b6
Dzmitry Rymarau <github@dzmitryrymarau.com>
2022-09-21 12:34:35 +0300

    refactor: DisposableEffect -> LaunchedEffect

--- sample/src/main/kotlin/com/pexip/sdk/sample/SampleTheme.kt
+++ sample/src/main/kotlin/com/pexip/sdk/sample/SampleTheme.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -27,12 +27,11 @@ fun SampleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable
         }
     }
     val systemUiController = rememberSystemUiController()
-    DisposableEffect(systemUiController, darkTheme) {
+    LaunchedEffect(systemUiController, darkTheme) {
         systemUiController.setSystemBarsColor(
             color = Color.Transparent,
             darkIcons = !darkTheme
         )
-        onDispose { }
     }
     MaterialTheme(colorScheme = colorScheme, content = content)
 }
