e03d0682b94430dd8506e6e65380c6f4358e0982
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-01-06 14:47:01 +0300

    feat: spotless (#598)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,41 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        google()
-    }
-    dependencies {
-        classpath(libs.android)
-        classpath(libs.dokka)
-        classpath(libs.kotlin)
-    }
-}
-
 plugins {
     alias(libs.plugins.dokka)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 group = checkNotNull(property("group")) { "group == null." }
 version = checkNotNull(property("version")) { "version == null." }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+spotless {
+    kotlin {
+        ratchetFrom = "origin/main"
+        target("**/*.kt")
+        targetExclude("**/build/**")
+        ktlint(libs.versions.ktlint.get())
+    }
+    kotlinGradle {
+        ratchetFrom = "origin/main"
+        target("**/*.gradle.kts")
+        targetExclude("**/build/**")
+        ktlint(libs.versions.ktlint.get())
+    }
+    format("misc") {
+        target(".gitignore", "*.md")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
 }
 
 tasks.dokkaHtmlMultiModule.configure {
     includes.from("README.md")
     val calendar = java.util.Calendar.getInstance(
         java.util.TimeZone.getTimeZone("UTC"),
-        java.util.Locale.ENGLISH
+        java.util.Locale.ENGLISH,
     )
     val footerMessage = "${calendar.get(java.util.Calendar.YEAR)} Pexip® AS, All rights reserved."
     val m = mapOf(
@@ -36,7 +43,8 @@ tasks.dokkaHtmlMultiModule.configure {
             |"footerMessage": "$footerMessage",
             |"customAssets": ["${file("dokka/pexip.svg")}"],
             |"customStyleSheets": ["${file("dokka/logo-styles.css")}"]
-            |}""".trimMargin()
+            |}
+        """.trimMargin(),
     )
     pluginsMapConfiguration.set(m)
 }
