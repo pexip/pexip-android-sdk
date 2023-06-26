f29dcb5429533249940356ddccd12b8f302e5037
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-06-26 18:12:57 +0300

    refactor(plugins): merge :android and :kotlin
    Keeping them separate doesn't give us any advantages

--- plugins/convention/build.gradle.kts
+++ plugins/convention/build.gradle.kts
@@ -4,6 +4,26 @@ plugins {
 
 gradlePlugin {
     plugins {
+        register("androidApplication") {
+            id = "com.pexip.sdk.android.application"
+            implementationClass = "com.pexip.sdk.AndroidApplicationPlugin"
+        }
+        register("androidLibrary") {
+            id = "com.pexip.sdk.android.library"
+            implementationClass = "com.pexip.sdk.AndroidLibraryPlugin"
+        }
+        register("kotlinAndroidApplication") {
+            id = "com.pexip.sdk.kotlin.android.application"
+            implementationClass = "com.pexip.sdk.KotlinAndroidApplicationPlugin"
+        }
+        register("kotlinAndroidLibrary") {
+            id = "com.pexip.sdk.kotlin.android.library"
+            implementationClass = "com.pexip.sdk.KotlinAndroidLibraryPlugin"
+        }
+        register("kotlinAndroidLibraryPublishing") {
+            id = "com.pexip.sdk.kotlin.android.library.publishing"
+            implementationClass = "com.pexip.sdk.KotlinAndroidLibraryPublishingPlugin"
+        }
         register("kotlinDokka") {
             id = "com.pexip.sdk.kotlin.dokka"
             implementationClass = "com.pexip.sdk.KotlinDokkaPlugin"
@@ -32,6 +52,8 @@ gradlePlugin {
 }
 
 dependencies {
+    implementation(libs.android)
+    implementation(libs.cachefix)
     implementation(libs.dokka)
     implementation(libs.dokka.base)
     implementation(libs.kotlin)
--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -14,5 +14,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "plugins"
-include(":android")
-include(":kotlin")
+include(":convention")
