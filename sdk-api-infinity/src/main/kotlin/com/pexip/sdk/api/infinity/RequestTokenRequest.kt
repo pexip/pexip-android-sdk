4d45ea81ab98e4bff1d2800569fa82ebeea16fca
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-08-08 15:06:19 +0300

    feat(api): add RequestTokenRequest.registrationToken
    To exchange a registration token for conference token we must use a this header instead of `token`.

--- sdk-api-infinity/src/main/kotlin/com/pexip/sdk/api/infinity/RequestTokenRequest.kt
+++ sdk-api-infinity/src/main/kotlin/com/pexip/sdk/api/infinity/RequestTokenRequest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Pexip AS
+ * Copyright 2022-2023 Pexip AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,4 +31,6 @@ public data class RequestTokenRequest(
     public val ssoToken: String? = null,
     @Transient
     public val incomingToken: String? = null,
+    @Transient
+    public val registrationToken: String? = null,
 )
