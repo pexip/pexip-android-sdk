69ca4ce5afc634e132ab8b69b9449a8d73143894
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-08-28 13:07:29 +0300

    feat: bump compileSdk to 34 (#189)
    `targetSdk` will be bumped a bit later (only affects the sample app) as
    it requires a few `Service`-related changes.

--- plugins/convention/src/main/kotlin/com/pexip/sdk/internal/Android.kt
+++ plugins/convention/src/main/kotlin/com/pexip/sdk/internal/Android.kt
@@ -21,7 +21,7 @@ internal object Android {
 
     const val minSdk = 21
     const val targetSdk = 33
-    const val compileSdk = 33
+    const val compileSdk = 34
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
