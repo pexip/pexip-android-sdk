d4b409a86e17193ed63b3d05463b7587c50d0918
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-08-29 12:01:24 +0300

    refactor: remove org.gradle.android.cache-fix plugin (#195)

--- plugins/convention/src/main/kotlin/com/pexip/sdk/AndroidApplicationPlugin.kt
+++ plugins/convention/src/main/kotlin/com/pexip/sdk/AndroidApplicationPlugin.kt
@@ -28,13 +28,11 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 
+@Suppress("UnstableApiUsage")
 class AndroidApplicationPlugin : Plugin<Project> {
 
     override fun apply(target: Project) = with(target) {
-        with(pluginManager) {
-            apply("com.android.application")
-            apply("org.gradle.android.cache-fix")
-        }
+        pluginManager.apply("com.android.application")
         configure<ApplicationExtension> {
             compileSdk = Android.compileSdk
             defaultConfig {
