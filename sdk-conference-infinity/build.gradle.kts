17506e99e761dcbf86a661eef906c9c998a53a5a
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-11-21 19:00:55 +0300

    refactor(conference): merge sdk-conference and sdk-conference-infinity

--- sdk-conference-infinity/build.gradle.kts
+++ sdk-conference-infinity/build.gradle.kts
@@ -1,14 +1,9 @@
 plugins {
     id("com.pexip.sdk.kotlin.jvm.publishing")
-    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     api(project(":sdk-conference"))
-    api(project(":sdk-api-infinity"))
-    implementation(libs.kotlinx.serialization.json.okio)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.turbine)
 }
 
 publishing.publications.withType<MavenPublication>().configureEach {
