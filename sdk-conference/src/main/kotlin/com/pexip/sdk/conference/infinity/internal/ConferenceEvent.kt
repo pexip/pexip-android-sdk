a28160bce6c4c608d67832643678633dbc4f38f2
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-11-29 17:01:57 +0300

    refactor(conference): bump MessageReceivedConferenceEvent deprecation level

--- sdk-conference/src/main/kotlin/com/pexip/sdk/conference/infinity/internal/ConferenceEvent.kt
+++ sdk-conference/src/main/kotlin/com/pexip/sdk/conference/infinity/internal/ConferenceEvent.kt
@@ -17,13 +17,11 @@ package com.pexip.sdk.conference.infinity.internal
 
 import com.pexip.sdk.api.Event
 import com.pexip.sdk.api.infinity.DisconnectEvent
-import com.pexip.sdk.api.infinity.MessageReceivedEvent
 import com.pexip.sdk.api.infinity.PresentationStartEvent
 import com.pexip.sdk.api.infinity.PresentationStopEvent
 import com.pexip.sdk.api.infinity.ReferEvent
 import com.pexip.sdk.conference.DisconnectConferenceEvent
 import com.pexip.sdk.conference.FailureConferenceEvent
-import com.pexip.sdk.conference.MessageReceivedConferenceEvent
 import com.pexip.sdk.conference.PresentationStartConferenceEvent
 import com.pexip.sdk.conference.PresentationStopConferenceEvent
 import com.pexip.sdk.conference.ReferConferenceEvent
@@ -39,13 +37,6 @@ internal inline fun ConferenceEvent(
         presenterName = event.presenterName,
     )
     is PresentationStopEvent -> PresentationStopConferenceEvent(at())
-    is MessageReceivedEvent -> MessageReceivedConferenceEvent(
-        at = at(),
-        participantId = event.participantId,
-        participantName = event.participantName,
-        type = event.type,
-        payload = event.payload,
-    )
     is ReferEvent -> ReferConferenceEvent(
         at = at(),
         conferenceAlias = event.conferenceAlias,
