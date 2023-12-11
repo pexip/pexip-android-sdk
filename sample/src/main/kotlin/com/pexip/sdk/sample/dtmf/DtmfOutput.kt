f82f011d6c8adf5b5aa0daa9ca2f012353623f89
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-12-11 16:08:11 +0300

    refactor(sample): remove DtmfAction typealias

--- sample/src/main/kotlin/com/pexip/sdk/sample/dtmf/DtmfOutput.kt
+++ sample/src/main/kotlin/com/pexip/sdk/sample/dtmf/DtmfOutput.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Pexip AS
+ * Copyright 2022-2023 Pexip AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,10 @@
  */
 package com.pexip.sdk.sample.dtmf
 
-sealed class DtmfOutput {
+sealed interface DtmfOutput {
 
-    class Tone(val tone: String) : DtmfOutput()
+    @JvmInline
+    value class Tone(val tone: String) : DtmfOutput
 
-    object Back : DtmfOutput() {
-
-        override fun toString(): String = "Back"
-    }
+    data object Back : DtmfOutput
 }
