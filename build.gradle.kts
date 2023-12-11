c850e06e27958525b43e906a6a00260787b181af
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-12-11 11:09:14 +0300

    refactor: switch back to kapt (#276)
    Some issues with Dagger not picking up Wire-generated classes after
    update to Kotlin 1.9.2x

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
 
 group = checkNotNull(property("group")) { "group == null." }
