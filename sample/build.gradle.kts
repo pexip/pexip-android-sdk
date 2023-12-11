aafee4a1893d46f136d2eefc6fe56ccca3d0603b
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2023-12-11 13:57:41 +0300

    refactor: reland ksp support
    This reverts commit c850e06e27958525b43e906a6a00260787b181af.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.pexip.sdk.licensee")
     alias(libs.plugins.dagger.hilt.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.wire)
 }
 
@@ -36,13 +36,6 @@ android {
     }
 }
 
-kapt {
-    correctErrorTypes = true
-    arguments {
-        arg("dagger.fastInit", "enabled")
-    }
-}
-
 wire {
     kotlin { }
 }
@@ -63,7 +56,7 @@ dependencies {
     implementation(libs.accompanist.systemuicontroller)
     implementation(libs.coil.compose)
     implementation(libs.dagger.hilt.android.runtime)
-    kapt(libs.dagger.hilt.compiler)
+    ksp(libs.dagger.hilt.compiler)
     implementation(libs.minidns.android21)
     implementation(libs.okhttp.logginginterceptor)
     implementation(libs.workflow.core.jvm)
