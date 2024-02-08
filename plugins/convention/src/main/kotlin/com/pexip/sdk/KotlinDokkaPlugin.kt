5731d13a229dbc3a639e3f1f6da193b56bb45455
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2024-02-08 12:33:53 +0300

    docs(media): add a sample for simple AudioDeviceManager usage (#305)
    Run `dokka` to generate the website and see the improved docs.

--- plugins/convention/src/main/kotlin/com/pexip/sdk/KotlinDokkaPlugin.kt
+++ plugins/convention/src/main/kotlin/com/pexip/sdk/KotlinDokkaPlugin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Pexip AS
+ * Copyright 2023-2024 Pexip AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,7 @@ class KotlinDokkaPlugin : Plugin<Project> {
         }
         tasks.withType<DokkaTaskPartial>().configureEach {
             dokkaSourceSets.configureEach {
+                samples.from("src/test/kotlin/Samples.kt")
                 includes.from("README.md")
                 externalDocumentationLink("https://kotlin.github.io/kotlinx.coroutines/")
                 noAndroidSdkLink.set(hasAndroidLibraryPluginProvider.map { !it })
