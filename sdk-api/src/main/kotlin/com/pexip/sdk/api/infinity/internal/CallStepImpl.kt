7b1d07ce16bc33e42e98c20fcf77c5bc5c7b0ddf
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2024-03-27 18:14:44 +0300

    fix(api): remove usage of okhttp3.internal.*
    This is required for smooth migration to 5.0.0.

--- sdk-api/src/main/kotlin/com/pexip/sdk/api/infinity/internal/CallStepImpl.kt
+++ sdk-api/src/main/kotlin/com/pexip/sdk/api/infinity/internal/CallStepImpl.kt
@@ -29,7 +29,6 @@ import com.pexip.sdk.api.infinity.UpdateResponse
 import kotlinx.serialization.SerializationException
 import okhttp3.Request
 import okhttp3.Response
-import okhttp3.internal.EMPTY_REQUEST
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 
