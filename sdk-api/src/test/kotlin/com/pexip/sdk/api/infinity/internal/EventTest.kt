607371fe86e96f7bf5b4c0caccf47e2f6db80166
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2024-07-30 17:33:54 +0300

    fixup! feat(api): add client_mute, client_unmute and is_client_muted

--- sdk-api/src/test/kotlin/com/pexip/sdk/api/infinity/internal/EventTest.kt
+++ sdk-api/src/test/kotlin/com/pexip/sdk/api/infinity/internal/EventTest.kt
@@ -227,6 +227,7 @@ internal class EventTest {
                         role = Role.GUEST,
                         serviceType = ServiceType.CONFERENCE,
                         callTag = "dd806491-eecc-42fc-accb-86a99448fc17",
+                        clientAudioMuted = false,
                     ),
                 ),
             )
@@ -251,6 +252,7 @@ internal class EventTest {
                         role = Role.UNKNOWN,
                         serviceType = ServiceType.CONFERENCE,
                         callTag = "dd806491-eecc-42fc-accb-86a99448fc17",
+                        clientAudioMuted = false,
                     ),
                 ),
             )
