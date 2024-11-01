6ccd1cf84cb18daec5e5f6f4015481ab95be5e07
Dzmitry Rymarau <dzmitry.rymarau@pexip.com>
2024-11-01 13:02:48 +0300

    refactor(sample): use enableEdgeToEdge instead of accompanist (#516)

--- sample/src/main/kotlin/com/pexip/sdk/sample/SampleActivity.kt
+++ sample/src/main/kotlin/com/pexip/sdk/sample/SampleActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Pexip AS
+ * Copyright 2022-2024 Pexip AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ class SampleActivity : AppCompatActivity() {
                 LocalEglBase provides eglBase,
                 LocalPermissionRationaleHelper provides permissionRationaleHelper,
             ) {
-                SampleTheme {
+                SampleTheme(systemBars = true) {
                     val rendering by sampleViewModel.rendering.collectAsState()
                     WorkflowRendering(
                         rendering = rendering,
